128c493961471ff43f03b19b5e051471c4ae0fdc
Shubh <shubhmahajan546@gmail.com>
2025-03-16 11:38:50 -0400

    Basic sign in page

---
+++ script.js
@@ -0,0 +1,34 @@
+// let test = document.getElementById("test"); 
+// test.addEventListener("mouseover", () => { 
+//     let style = test.style; 
+//     style.backgroundColor = "blue"
+//     style.boxShadow = "5px 5px 20px blue"
+
+// }); 
+
+// test.addEventListener("mouseleave", () => { 
+//     test.style.backgroundColor = "coral"
+//     test.style.boxShadow = "5px 5px 20px coral" 
+    
+// }); 
+
+// test.addEventListener("click", () => { 
+    
+//     let style = test.style; 
+//     style.backgroundColor = "green"
+//     style.boxShadow = "5px 5px 20px green"
+    
+//     test.addEventListener("mousemove", () => { 
+//         style.backgroundColor = "blue"
+//         style.boxShadow = "5px 5px 20px blue"
+//     }); 
+
+//     document.getElementById("button").style.visibility = "hidden"
+//     document.getElementById("div_button").style.visibility = "hidden"
+//     document.getElementById("message").style.visibility = "visible"
+
+//     setTimeout(() => { 
+//         document.getElementById("message").style.visibility = "hidden"
+//     }, 1000)
+
+// }); 
